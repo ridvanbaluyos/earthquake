a09f12fca006d931f9bbfdb695976b840af47c99
Ridvan Baluyos <ridvan@baluyos.net>
2017-04-20 01:56:55 +0800

    protocol agnostic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
             <h1>Hello there!</h1>
             <p>This dashboard is still under construction. There's not much content yet but it will be populated soon with fancy graphs, numerics, references, etc. </p>
             <p>For the meantime, you might want to check the <a href="/earthquake-history">past earthquakes.</a></p>
-            <a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>
+            <a href="//thecatapi.com"><img src="//thecatapi.com/api/images/get?format=src&type=gif"></a>
 
         </div>
     </div>
