7d45dc265cc7e553e1722128a412cc43ba8bdd5b
Ridvan Baluyos <ridvan@baluyos.net>
2017-04-21 02:02:47 +0800

    add google analytics

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -71,5 +71,16 @@
 
 <!-- Page Specific JS -->
 @yield('js-page-specific')
+
+<!-- Analytics -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-45399063-5', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
