57dc96600c189a723c096fcf5eb616b4d0892af8
Ridvan Baluyos <ridvan@baluyos.net>
2017-04-23 16:43:34 +0800

    typoe

--- app/Console/Commands/CheckEarthquakes.php
+++ app/Console/Commands/CheckEarthquakes.php
@@ -133,7 +133,7 @@ class CheckEarthquakes extends Command
             'apikey' => config('app.semaphore_api_key'),
             'number' => config('app.my_number'),
             'message' => $message,
-            'sendername' => confg('app.semaphore_sender')
+            'sendername' => config('app.semaphore_sender')
         );
         curl_setopt( $ch, CURLOPT_URL,'http://api.semaphore.co/api/v4/messages' );
         curl_setopt( $ch, CURLOPT_POST, 1 );
