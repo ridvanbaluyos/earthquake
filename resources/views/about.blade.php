525a4ba685dbcc8fd155b8de2c11d668d9373d98
Ridvan Baluyos <ridvan@baluyos.net>
2017-07-14 14:51:59 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,47 +13,6 @@
                 Earthquake Philippines is a side-project by <a href="//ridvanbaluyos.com" target="_blank" title="Ridvan Baluyos">Ridvan Baluyos</a>. This site is currently on incremental development.
             </p>
             <hr>
-            <h5>Tech Stack</h5>
-            <ul>
-                <li>
-                    <a href="//laravel.com/" target="_blank">Laravel PHP Framework</a> - The PHP Framework For Web Artisans.
-                </li>
-                <li>
-                    <a href="//getbootstrap.com/" target="_blank">Bootstrap</a> - Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.
-                </li>
-                <li>
-                    <a href="//www.chartjs.org/" target="_blank">Charts.js</a> - Simple yet flexible JavaScript charting for designers & developers.
-                </li>
-                <li>
-                    <a href=//developers.google.com/maps/" target="_blank">Google Maps API</a> - Millions of websites and apps use Google Maps APIs to power location experiences for their users.
-                </li>
-            </ul>
-            <hr>
-            <h5>Infrastructure</h5>
-            <ul>
-                <li>
-                    <a href="//aws.amazon.com/ec2/" target="_blank">Amazon EC2</a> - Amazon Elastic Compute Cloud (Amazon EC2) is a web service that provides secure, resizable compute capacity in the cloud.
-                </li>
-                <li>
-                    <a href="//aws.amazon.com/elasticache/" target="_blank">Amazon ElastiCache</a> - Amazon ElastiCache is a web service that makes it easy to deploy, operate, and scale an in-memory data store or cache in the cloud.
-                </li>
-                <li>
-                    <a href="//semaphore.co/" target="_blank">Semaphore</a> - Semaphore lets you send an SMS with a single line of code. No complicated setup, no dealing with telecom protocols and procedures.
-                </li>
-                <li>
-                    <a href="//http://mailgun.com/" target="_blank">Mailgun</a> - Transactional Email API Service for Developers.
-                </li>
-                <li>
-                    <a href="//www.cloudflare.com" target="_blank">Cloudflare</a> - Cloudflare speeds up and protects millions of websites, APIs, SaaS services, and other properties connected to the Internet.
-                </li>
-                <li>
-                    <a href="//newrelic.com" target="_blank">New Relic</a> - Digital Performance Monitoring and Management.
-                </li>
-                <li>
-                    <a href="//mixpanel.com" target="_blank">Mixpanel</a> - Product analytics for mobile, web, and beyond.
-                </li>
-            </ul>
-            <hr>
             <h5>Data Source</h5>
             <ul>
                 <li>
