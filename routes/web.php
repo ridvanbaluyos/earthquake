c1b259e8fe3fd73ca27139cd681c5832f143b7a7
Eric Bangug <ericbangug@gmail.com>
2017-10-27 12:02:33 +0800

    Refactor routes: use ::match() so that multiple http verbs that points to the same controller only uses one line

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,15 @@
 |
 */
 
-Route::get('/', 'HomeController@getIndex');
-Route::post('/', 'HomeController@getIndex');
-Route::get('/earthquakes', 'HomeController@getEarthquakeHistory');
-Route::post('/earthquakes', 'HomeController@getEarthquakeHistory');
+Route::match(['get', 'post'], '/', 'HomeController@getIndex');
+
+Route::match(['get', 'post'], '/earthquakes', 'HomeController@getEarthquakeHistory');
 
 Route::get('/earthquakes/{id}', 'HomeController@getEarthquakeDetails');
 
-Route::get('/earthquake-heatmap', 'HomeController@getHeatmap');
-Route::post('/earthquake-heatmap', 'HomeController@getHeatmap');
+Route::match(['get', 'post'], '/earthquake-heatmap', 'HomeController@getHeatmap');
 
-Route::get('/earthquake-graphs-charts', 'HomeController@getGraphCharts');
-Route::post('/earthquake-graphs-charts', 'HomeController@getGraphCharts');
+Route::match(['get', 'post'], '/earthquake-graphs-charts', 'HomeController@getGraphCharts');
 
 Route::get('/earthquake-101', 'HomeController@getEarthquake101');
 
@@ -32,10 +29,7 @@ Route::get('/about', 'HomeController@getAbout');
 
 Route::get('/test', 'HomeController@getTest');
 
-
 Route::prefix('amp')->group(function () {
     Route::get('/', 'HomeController@getIndex')
         ->name('amp-homepage');
-
-    
 });
